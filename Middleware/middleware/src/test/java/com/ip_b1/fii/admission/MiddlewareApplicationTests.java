c4d50107be9b076632214e4d8a9729b6f94b9602
ted_996 <ebudaca@ias.bitdefender.biz>
2017-05-05 17:12:40 +0300

    Maybe working Maven?

---
+++ Middleware/middleware/src/test/java/com/ip_b1/fii/admission/MiddlewareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ip_b1.fii.admission;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiddlewareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
