d2fcc527bdbc7bf417a7118076f7b657b3803846
griggson <mariusgrygore@gmail.com>
2017-05-07 22:50:00 +0300

    Added WithdrawApplication class

--- Middleware/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/WithdrawApplication.java
+++ Middleware/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/WithdrawApplication.java
@@ -31,6 +31,7 @@ public class WithdrawApplication {
         return new ResponseEntity<>(new SuccessEntity(true), HttpStatus.OK);
     }
 
+
     private static boolean process(String sessionId, String cnp)
     {
         RestTemplate restTemplate = new RestTemplate();
