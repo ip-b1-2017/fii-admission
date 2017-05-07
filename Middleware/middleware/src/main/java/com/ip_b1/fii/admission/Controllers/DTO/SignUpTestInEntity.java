e3172fc84134a06ee5f408debe4b4a1495fe0278
Domnaru Alexandru <domnaru.alexandru@gmail.com>
2017-05-07 14:08:07 +0300

    Added signup class and created DTOs for it

---
+++ Middleware/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/DTO/SignUpTestInEntity.java
@@ -0,0 +1,23 @@
+package com.ip_b1.fii.admission.Controllers.DTO;
+
+public class SignUpTestInEntity {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
