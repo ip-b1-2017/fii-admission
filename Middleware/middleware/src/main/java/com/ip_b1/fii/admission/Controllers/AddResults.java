310bb556a19b904d6ef10561d90b046a7f45c9bd
Andrei <riscanuandrei@yahoo.com>
2017-05-07 20:38:15 +0300

    Modified results function

--- Middleware/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/AddResults.java
+++ Middleware/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/AddResults.java
@@ -3,6 +3,7 @@ package com.ip_b1.fii.admission.Controllers;
 import com.ip_b1.fii.admission.DTO.AuthEntity;
 import com.ip_b1.fii.admission.DTO.NotificationsOutEntity;
 import com.ip_b1.fii.admission.DTO.ResultInEntity;
+import com.ip_b1.fii.admission.DTO.SuccessEntity;
 import com.ip_b1.fii.admission.ServerProperties;
 import com.ip_b1.fii.admission.Utils.AuthUtils;
 import org.springframework.http.HttpStatus;
@@ -15,18 +16,18 @@ import org.springframework.web.client.RestTemplate;
 public class AddResults {
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<String> setResult(@RequestBody ResultInEntity result) {
+    public ResponseEntity<SuccessEntity> setResult(@RequestBody ResultInEntity result) {
         if (!AuthUtils.checkAuth(result.getAuth()) || !AuthUtils.checkAuthIsAdmin(result.getAuth())) {
             return new ResponseEntity<>(
-                    new String("UNAUTHORIZED"),
+                    new SuccessEntity(false),
                     HttpStatus.UNAUTHORIZED
             );
         } else {
             RestTemplate template = new RestTemplate();
 
-            ResponseEntity<String> entity = template.getForEntity(
+            ResponseEntity<SuccessEntity> entity = template.getForEntity(
                     ServerProperties.modelUrl + "/set_result",
-                    String.class
+                    SuccessEntity.class
             );
 
             return new ResponseEntity<>(
