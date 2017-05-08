17656c2a87078950a71bd1e5f32287b2738a68bb
Ionut Octavian ILIE <iilie@bitdefender.com>
2017-05-08 15:06:27 +0300

    Added all CRUD operations for Candidati

---
+++ BackEnd/spring-boot/src/main/java/fii/admission/candidati/Candidat.java
@@ -0,0 +1,41 @@
+package fii.admission.candidati;
+
+public class Candidat {
+	String CNP;
+	String telefon;
+	String nume;
+	String prenume;
+	String useremail;
+	
+	public String getTelefon() {
+		return telefon;
+	}
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public String getPrenume() {
+		return prenume;
+	}
+	public void setPrenume(String prenume) {
+		this.prenume = prenume;
+	}
+	public String getCNP() {
+		return CNP;
+	}
+	public void setCNP(String CNP) {
+		this.CNP = CNP;
+	}
+	public String getUserEmail() {
+		return useremail;
+	}
+	public void setUserEmail(String useremail) {
+		this.useremail = useremail;
+	}
+}
+
---
+++ BackEnd/spring-boot/src/main/java/fii/admission/candidati/CandidatService.java
@@ -0,0 +1,126 @@
+package fii.admission.candidati;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+import org.springframework.stereotype.Service;
+
+import fii.admission.MainApp;
+
+@Service
+public class CandidatService {
+	
+	static public List<Candidat> getAllCandidat() {
+		ArrayList<Candidat> result = new ArrayList<Candidat>();
+		Connection con = MainApp.getDBConnection();
+		String query = "SELECT * FROM CANDIDAT";
+		try{
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				Candidat candidat = new Candidat();
+				candidat.setCNP(rs.getString("CNP"));
+				candidat.setTelefon(rs.getString("TELEFON"));
+				candidat.setNume(rs.getString("NUME"));
+				candidat.setPrenume(rs.getString("PRENUME"));
+				candidat.setUserEmail(rs.getString("USEREMAIL"));
+				result.add(candidat);
+			}
+			stmt.close();
+			rs.close();
+			if(result.isEmpty()) return null;
+			else return result;
+		} catch(Exception exc) {
+			System.out.printf("[error][getAllCandidat] %s\n", exc.getMessage());
+		}
+		return null;
+	}
+	
+	public static Candidat getCandidat(String cnp) {
+		Candidat result = new Candidat();
+		Connection con = MainApp.getDBConnection();
+		String query = "SELECT * FROM CANDIDAT WHERE CNP = ?";
+		try{
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setString(1, cnp);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result.setCNP(rs.getString("CNP"));
+				result.setTelefon(rs.getString("TELEFON"));
+				result.setNume(rs.getString("NUME"));
+				result.setPrenume(rs.getString("PRENUME"));
+				result.setUserEmail(rs.getString("USEREMAIL"));
+			}
+			else return null;
+			pstmt.close();
+			rs.close();
+			return result;
+		} catch(Exception exc) {
+			System.out.printf("[error][getCandidat] %s\n", exc.getMessage());
+		}
+		return null;
+	}
+	
+	public static int updateCandidat(String cnp, Candidat candidat) {
+		int result;
+		Connection con = MainApp.getDBConnection();
+		String query = "UPDATE CANDIDAT SET nume = ?, prenume = ?, cnp = ?, " + 
+					   "useremail = ?, telefon = ? where cnp = ?";
+			
+		try{
+			PreparedStatement pstmt = con.prepareStatement(query.toString());
+			pstmt.setString(1, candidat.getNume());
+			pstmt.setString(2, candidat.getPrenume());
+			pstmt.setString(3, candidat.getCNP());
+			pstmt.setString(4, candidat.getUserEmail());
+			pstmt.setString(5, candidat.getTelefon());			
+			pstmt.setString(6, cnp);
+			result = pstmt.executeUpdate();
+			pstmt.close();
+			return result;
+		} catch(Exception exc) {
+			System.out.printf("[error][updateCandidat] %s\n", exc.getMessage());
+		}
+		return 0;
+	}
+	
+	public static int deleteCandidat(String cnp) {
+		int result;
+		Connection con = MainApp.getDBConnection();
+		String query = "DELETE FROM CANDIDAT WHERE cnp = ?";
+		try{
+			PreparedStatement pstmt = con.prepareStatement(query.toString());
+			pstmt.setString(1, cnp);
+			result = pstmt.executeUpdate();
+			pstmt.close();
+			return result;
+		} catch(Exception exc) {
+			System.out.printf("[error][deleteCandidat] %s\n", exc.getMessage());
+		}
+		return 0;
+	}
+	
+	public static int insertCandidat(Candidat candidat) {
+		int result;
+		Connection con = MainApp.getDBConnection();
+		String query = "INSERT INTO CANDIDAT "
+				+ "(NUME, PRENUME, CNP, USEREMAIL, TELEFON)"
+				+ "VALUES ( ?, ?, ?, ?, ?)";		
+		
+		try{
+			PreparedStatement pstmt = con.prepareStatement(query.toString());
+			pstmt.setString(1, candidat.getNume());
+			pstmt.setString(2, candidat.getPrenume());
+			pstmt.setString(3, candidat.getCNP());
+			pstmt.setString(4, candidat.getUserEmail());
+			pstmt.setString(5, candidat.getTelefon());
+			result = pstmt.executeUpdate();
+			pstmt.close();
+			return result;
+		} catch(Exception exc) {
+			System.out.printf("[error][updateCandidat] %s\n", exc.getMessage());
+		}
+		return 0;
+	}
+}
