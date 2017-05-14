f677c6740a773ace801cda785f5b8d61ad81422e
costeltuiu <costel.tuiu@info.uaic.ro>
2017-05-14 23:49:00 +0300

    GET candidates list

---
+++ Integration/Mare_Proiect/middleware/src/main/java/com/ip_b1/fii/admission/DTO/CandidatEntity.java
@@ -0,0 +1,39 @@
+package com.ip_b1.fii.admission.DTO;
+
+public class CandidatEntity {
+    private String nume;
+    private String prenume;
+    private String CNP;
+    private String useremail;
+
+    public CandidatEntity(){
+
+    }
+
+    public String getNume() {
+        return nume;
+    }
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+    public String getPrenume() {
+        return prenume;
+    }
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+    public String getCNP() {
+        return CNP;
+    }
+    public void setCNP(String CNP) {
+        this.CNP = CNP;
+    }
+    public String getUserEmail() {
+        return useremail;
+    }
+    public void setUserEmail(String useremail) {
+        this.useremail = useremail;
+    }
+}
+
+
