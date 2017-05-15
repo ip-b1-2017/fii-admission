4e31edf5d61fea4b56be0a686627a24dbf46e3ec
MaximDragosAndrei <andrei.maxim97@gmail.com>
2017-05-15 03:18:46 +0300

    Update drop_triggers.sql
    Drop email & candidat update triggers

--- BackEnd/spring-boot/gen_db_new/drop_triggers.sql
+++ BackEnd/spring-boot/gen_db_new/drop_triggers.sql
@@ -1,7 +1,10 @@
 DROP TRIGGER Sali_Delete_Trigger;
 DROP TRIGGER Examen_Delete_Trigger;
-DROP TRIGGER Candidat_Delete_Trigger
-DROP TRIGGER Note_Delete_Trigger
-DROP TRIGGER Medie_Delete_Trigger
-DROP TRIGGER Notificari_Delete_Trigger
-DROP TRIGGER Form_Delete_Trigger
+DROP TRIGGER Candidat_Delete_Trigger;
+DROP TRIGGER Note_Delete_Trigger;
+DROP TRIGGER Medie_Delete_Trigger;
+DROP TRIGGER Notificari_Delete_Trigger;
+DROP TRIGGER Form_Delete_Trigger;
+
+DROP TRIGGER Candidat_Update_Trigger;
+DROP TRIGGER Examen_Update_Trigger;
