08c365a0dce7789faa79699c54a3c2af2fa80eeb
samson-georgiana <georgianasam2007@yahoo.com>
2017-05-15 11:29:51 +0300

    Update USER.sql

--- BackEnd/spring-boot/gen_db_new/USER.sql
+++ BackEnd/spring-boot/gen_db_new/USER.sql
@@ -2,4 +2,4 @@ INSERT INTO USER ('ROL','EMAIL','PAROLA','TOKEN','FIRSTNAME','LASTNAME') VALUES 
 INSERT INTO USER ('ROL','EMAIL','PAROLA','TOKEN','FIRSTNAME','LASTNAME') VALUES ('Elev','felis.Donec@semper.co.uk','feugiat','Cum sociis','Noah','Wynter');
 INSERT INTO USER ('ROL','EMAIL','PAROLA','TOKEN','FIRSTNAME','LASTNAME') VALUES ('Elev','luctus@nisiCumsociis.net','eu','quis','Yael','Dana');
 INSERT INTO USER ('ROL','EMAIL','PAROLA','TOKEN','FIRSTNAME','LASTNAME') VALUES ('Elev','turpis.egestas.Aliquam@dictum.edu','odio','eget','Elaine','Xaviera');
-INSERT INTO USER ('ROL','EMAIL','PAROLA','TOKEN','FIRSTNAME','LASTNAME') VALUES ('Elev','orci.consectetuer.euismod@Aeneangravida.com','tortor,','porttitor vulputate, posuere',\,'Chandler','Jakeem');
+INSERT INTO USER ('ROL','EMAIL','PAROLA','TOKEN','FIRSTNAME','LASTNAME') VALUES ('Elev','orci.consectetuer.euismod@Aeneangravida.com','tortor,','porttitor vulputate, posuere','Chandler','Jakeem');
