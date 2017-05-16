374be041e4bbbdebe8e91bf83f9a50d7fd8d8820
Ionut Octavian ILIE <iilie@bitdefender.com>
2017-05-16 22:46:39 +0300

    added package for candidat&form

--- Integration/Mare_Proiect/FII-ADMISSION/src/main/resources/static/JS/get_candidati.js
+++ Integration/Mare_Proiect/FII-ADMISSION/src/main/resources/static/JS/get_candidati.js
@@ -1,2 +1,6 @@
 $(document).ready(function () {
-}};
+        $.getJSON( "http://localhost:9999/model/candidati_formuri", {})
+          .done(function( json ) {
+            console.log( "JSON Data: " + json[0] );
+          })
+});
--- Integration/Mare_Proiect/spring-boot/src/main/java/fii/admission/candidati_forms/CandidatFormController.java
+++ Integration/Mare_Proiect/spring-boot/src/main/java/fii/admission/candidati_forms/CandidatFormController.java
@@ -1,21 +1,21 @@
 package fii.admission.candidati_forms;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping(value = "/model")
 @RestController
 public class CandidatFormController {
     @RequestMapping(value = "/candidati_formuri", method = RequestMethod.GET)
-    public ResponseEntity<List<CandidatForm>> getAllUser() {
+    public ResponseEntity<List<CandidatForm>> getAllCandidatiAndForms() {
+        System.out.print("[debug][getAllCandidatiAndForms] ");
         List<CandidatForm> result = CandidatFormService.getAllCandidatForm();
+        System.out.println(result.size() + " results");
 
         if (result == null)
             return new ResponseEntity<List<CandidatForm>>(result, HttpStatus.NO_CONTENT);
