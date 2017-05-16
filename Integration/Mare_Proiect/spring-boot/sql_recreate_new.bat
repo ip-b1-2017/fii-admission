464751430151f9d210f74ceeb0b4542e6d18ec7a
Ionut Octavian ILIE <iilie@bitdefender.com>
2017-05-17 00:07:46 +0300

    removed not null constraints from profesori table

--- Integration/Mare_Proiect/spring-boot/sql_recreate_new.bat
+++ Integration/Mare_Proiect/spring-boot/sql_recreate_new.bat
@@ -10,4 +10,3 @@ sqlite3 FiiAdmission.db < gen_db_new\MEDIE.sql
 sqlite3 FiiAdmission.db < gen_db_new\SALI.sql
 sqlite3 FiiAdmission.db < gen_db_new\SALI_EXAMEN.sql
 sqlite3 FiiAdmission.db < gen_db_new\FORM.sql
-sqlite3 FiiAdmission.db < gen_db_new\STUDENTI.SQL
