98b5cf51004ae2dce963b4f33104fde55f2dc06a
costeltuiu <costel.tuiu@info.uaic.ro>
2017-05-17 00:03:30 +0300

    Professors half insert, full get, no delete

---
+++ Integration/Mare_Proiect/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/GetProfessors.java
@@ -0,0 +1,36 @@
+package com.ip_b1.fii.admission.Controllers;
+
+import com.ip_b1.fii.admission.DTO.CandidatEntity;
+import com.ip_b1.fii.admission.DTO.ProfessorEntity;
+import com.ip_b1.fii.admission.ServerProperties;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/controller/get_professors")
+public class GetProfessors {
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ProfessorEntity>> run() {
+
+        RestTemplate template = new RestTemplate();
+
+        ResponseEntity<List<ProfessorEntity>> professorResponse =
+                template.exchange(ServerProperties.modelUrl + "/profesori",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<ProfessorEntity>>() {
+                        });
+        List<ProfessorEntity> professors = professorResponse.getBody();
+
+        return new ResponseEntity<List<ProfessorEntity>>(
+                professors,
+                HttpStatus.OK
+        );
+    }
+}
---
+++ Integration/Mare_Proiect/middleware/src/main/java/com/ip_b1/fii/admission/Controllers/InsertProfessor.java
@@ -0,0 +1,45 @@
+package com.ip_b1.fii.admission.Controllers;
+
+import com.ip_b1.fii.admission.DTO.ProfessorEntity;
+import com.ip_b1.fii.admission.DTO.ResultInEntity;
+import com.ip_b1.fii.admission.DTO.SuccessEntity;
+import com.ip_b1.fii.admission.ServerProperties;
+import com.ip_b1.fii.admission.Utils.AuthUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/controller/add_professor")
+public class InsertProfessor {
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<ProfessorEntity> setResult(@RequestBody ProfessorEntity professor) {
+        ProfessorEntity professorToAdd = new ProfessorEntity();
+        professorToAdd.setNume(professor.getNume());
+        professorToAdd.setPrenume(professor.getPrenume());
+        professorToAdd.setPCNP(professor.getPCNP());
+        professorToAdd.setSali_examenexamenid(professor.getSali_examenexamenid());
+        professorToAdd.setSali_examensaliid(professor.getSali_examensaliid());
+
+        RestTemplate template = new RestTemplate();
+
+        template.put(
+                ServerProperties.modelUrl + "/profesori",
+                professorToAdd);
+/*
+        ResponseEntity<String> response = template.exchange(ServerProperties.modelUrl + "/profesori",
+                HttpMethod.PUT, ProfessorEntity, ProfessorEntity.class, professorToAdd);
+*/
+        return new ResponseEntity<ProfessorEntity>(
+                //response,
+                HttpStatus.OK
+        );
+    }
+    }
