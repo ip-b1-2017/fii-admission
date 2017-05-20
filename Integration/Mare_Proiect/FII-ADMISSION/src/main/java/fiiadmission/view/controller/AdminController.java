65a28a4772a0732996611c50b5d43e44c942db03
costeltuiu <costel.tuiu@info.uaic.ro>
2017-05-20 19:19:52 +0300

    add si delete professor

--- Integration/Mare_Proiect/FII-ADMISSION/src/main/java/fiiadmission/view/controller/AdminController.java
+++ Integration/Mare_Proiect/FII-ADMISSION/src/main/java/fiiadmission/view/controller/AdminController.java
@@ -1,6 +1,9 @@
 package fiiadmission.view.controller;
 
 import com.sun.deploy.net.HttpResponse;
+import fiiadmission.ServerProperties;
+import fiiadmission.dto.ProfessorEntity;
+import fiiadmission.dto.SuccessReasonEntity;
 import io.swagger.models.Model;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.core.ParameterizedTypeReference;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import validator.IValidator;
+import validator.Mapper;
 import validator.Validator;
 import fiiadmission.view.Model.*;
 
@@ -31,17 +35,35 @@ import java.util.Map;
 public class AdminController {
 
     IValidator validator = new Validator();
-    @RequestMapping(value = "/delete_teacher", method = RequestMethod.GET)
-    public ModelAndView getTeachers(HttpServletRequest req, HttpServletResponse rep){
-        return new ModelAndView("profesori");
-    }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public void save(HttpServletRequest req){
+    @RequestMapping(value = "/operation", method = RequestMethod.POST)
+    public ModelAndView operation(HttpServletRequest req){
         Map map = req.getParameterMap();
-        if(validator.isValid(map)){
-            //return 
+
+        Map<String, String> test = Mapper.changeToSingle(map);
+        RestTemplate template = new RestTemplate();
+        if (test.get("button").equals("delete")) {
+            ResponseEntity<SuccessReasonEntity> nume = template.postForEntity(ServerProperties.middleUrl + "/remove_professor",
+                    test.get("pcnp"), SuccessReasonEntity.class);
+            return new ModelAndView("redirect:/delete_teacher");
         }
+        if (test.get("button").equals("add")) {
+            test.remove("button");
+            ResponseEntity<SuccessReasonEntity> nume = template.postForEntity(ServerProperties.middleUrl + "/add_professor",
+                    test, SuccessReasonEntity.class);
+            return new ModelAndView("redirect:/delete_teacher");
+        }
+
+        if (test.get("button").equals("see teachers")) {
+            ResponseEntity<List<ProfessorEntity>> professorResponse =
+                    template.exchange(ServerProperties.middleUrl + "/get_professors",
+                            HttpMethod.POST, null, new ParameterizedTypeReference<List<ProfessorEntity>>() {
+                            });
+            List<ProfessorEntity> professors = professorResponse.getBody();
+            return new ModelAndView("redirect:/tabel_profesori");
+        }
+
+        return  new ModelAndView("redirect:/delete_teacher");
     }
 
     @RequestMapping(value = "/candidati", method = RequestMethod.GET)
