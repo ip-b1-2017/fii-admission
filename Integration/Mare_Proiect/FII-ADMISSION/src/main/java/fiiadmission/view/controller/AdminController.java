4e5f409b447a49b98816adc1b476308d274bc2f2
costeltuiu <costel.tuiu@info.uaic.ro>
2017-05-21 15:37:14 +0300

    Finished professors and classrooms

--- Integration/Mare_Proiect/FII-ADMISSION/src/main/java/fiiadmission/view/controller/AdminController.java
+++ Integration/Mare_Proiect/FII-ADMISSION/src/main/java/fiiadmission/view/controller/AdminController.java
@@ -3,6 +3,7 @@ package fiiadmission.view.controller;
 import com.sun.deploy.net.HttpResponse;
 import fiiadmission.ServerProperties;
 import fiiadmission.dto.AuthEntity;
+import fiiadmission.dto.ClassroomEntity;
 import fiiadmission.dto.ProfessorEntity;
 import fiiadmission.dto.SuccessReasonEntity;
 import io.swagger.models.Model;
@@ -33,8 +34,8 @@ public class AdminController {
 
     IValidator validator = new Validator();
 
-    @RequestMapping(value = "/operation", method = RequestMethod.POST)
-    public ModelAndView operation(HttpServletRequest req){
+    @RequestMapping(value = "/teachers_operation", method = RequestMethod.GET)
+    public ModelAndView teachersOperation(HttpServletRequest req){
         Map map = req.getParameterMap();
 
         Map<String, String> test = Mapper.changeToSingle(map);
@@ -42,13 +43,13 @@ public class AdminController {
         if (test.get("button").equals("delete")) {
             ResponseEntity<SuccessReasonEntity> nume = template.postForEntity(ServerProperties.middleUrl + "/remove_professor",
                     test.get("pcnp"), SuccessReasonEntity.class);
-            return new ModelAndView("redirect:/operation");
+            return new ModelAndView("redirect:/teachers");
         }
         if (test.get("button").equals("add")) {
             test.remove("button");
             ResponseEntity<SuccessReasonEntity> nume = template.postForEntity(ServerProperties.middleUrl + "/add_professor",
                     test, SuccessReasonEntity.class);
-            return new ModelAndView("redirect:/operation");
+            return new ModelAndView("redirect:/teachers");
         }
 
         if (test.get("button").equals("see teachers")) {
@@ -57,10 +58,46 @@ public class AdminController {
                             HttpMethod.POST, null, new ParameterizedTypeReference<List<ProfessorEntity>>() {
                             });
             List<ProfessorEntity> professors = professorResponse.getBody();
-            return new ModelAndView("redirect:/operation");
+            ModelAndView modelAndView = new ModelAndView("/tabel_profesori");
+            modelAndView.addObject("teachers", professors);
+
+            return modelAndView;
         }
 
-        return  new ModelAndView("redirect:/operation");
+        return  new ModelAndView("redirect:/teachers");
+    }
+
+    @RequestMapping(value = "/classroom_operations", method = RequestMethod.GET)
+    public ModelAndView classroom_operations(HttpServletRequest req){
+        Map map = req.getParameterMap();
+
+        Map<String, String> test = Mapper.changeToSingle(map);
+        RestTemplate template = new RestTemplate();
+        if (test.get("button").equals("Stergere clasa")) {
+            ResponseEntity<SuccessReasonEntity> nume = template.postForEntity(ServerProperties.middleUrl + "/remove_classroom",
+                    test.get("id"), SuccessReasonEntity.class);
+            return new ModelAndView("redirect:/classroom");
+        }
+        if (test.get("button").equals("Adaugare clasa")) {
+            test.remove("button");
+            ResponseEntity<SuccessReasonEntity> nume = template.postForEntity(ServerProperties.middleUrl + "/add_classroom",
+                    test, SuccessReasonEntity.class);
+            return new ModelAndView("redirect:/classroom");
+        }
+
+        if (test.get("button").equals("Vizualizare sali")) {
+            ResponseEntity<List<ClassroomEntity>> classroomResponse =
+                    template.exchange(ServerProperties.middleUrl + "/get_classrooms",
+                            HttpMethod.POST, null, new ParameterizedTypeReference<List<ClassroomEntity>>() {
+                            });
+            List<ClassroomEntity> classrooms = classroomResponse.getBody();
+            ModelAndView modelAndView = new ModelAndView("/classroom_table");
+            modelAndView.addObject("classrooms", classrooms);
+
+            return modelAndView;
+        }
+
+        return  new ModelAndView("redirect:/classroom");
     }
 
     @RequestMapping(value = "/candidates_admin", method = RequestMethod.GET)
