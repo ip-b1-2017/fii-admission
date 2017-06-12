0bf625be89369fee6c733ec984ce734f012d329a
ted_996 <ebudaca@ias.bitdefender.biz>
2017-06-12 23:26:14 +0300

    Work on see notifications

--- Integration/Mare_Proiect/FII-ADMISSION/src/main/java/fiiadmission/view/controller/NotificationController.java
+++ Integration/Mare_Proiect/FII-ADMISSION/src/main/java/fiiadmission/view/controller/NotificationController.java
@@ -1,33 +1,30 @@
 package fiiadmission.view.controller;
 
+
 import fiiadmission.ServerProperties;
 import fiiadmission.dto.AuthEntity;
 import fiiadmission.dto.Email;
-import fiiadmission.dto.StatisticiUser;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class DashboardUserController {
-
-    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+public class NotificationController {
+    @RequestMapping()
     public @ResponseBody
     ModelAndView getDashboard(String error, Model model, HttpServletRequest req, HttpServletResponse rep) throws IOException {
         AuthEntity auth = AuthEntity.fromCookies(req.getCookies());
@@ -37,16 +34,13 @@ public class DashboardUserController {
         }
         Email email = new Email();
         email.setEmail(auth.getUsername());
-        StatisticiUser statisticiUser = getStatisticiUser(email);
         List<String> notifications = getNotifications(auth);
 
-        if (statisticiUser == null || notifications == null){
+        if (notifications == null){
             rep.sendError(400, "Bad authentication.");
             return null;
         }
 
-        model.addAttribute("nr_aplicatii_depuse", statisticiUser.getNumarAplicanti());
-        model.addAttribute("status_aplicatie",statisticiUser.getStatusAplicatie());
         model.addAttribute("user_name",auth.getUsername());
         model.addAttribute("notifications", notifications);
         return new ModelAndView("/dashboard");
@@ -72,22 +66,4 @@ public class DashboardUserController {
             return null;
         }
     }
-
-    private StatisticiUser getStatisticiUser(Email email){
-        RestTemplate restTemplate = new RestTemplate();
-
-        try {
-            ResponseEntity<StatisticiUser> response = restTemplate.postForEntity(
-                    ServerProperties.middleUrl + "/statistici/get_statistici_user",
-                    email,
-                    StatisticiUser.class
-            );
-            return response.getBody();
-        }
-        catch(RestClientResponseException ex){
-            ex.printStackTrace();
-            return null;
-        }
-    }
-
 }
